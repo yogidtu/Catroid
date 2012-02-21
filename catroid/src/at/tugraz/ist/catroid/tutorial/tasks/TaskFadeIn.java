1b81db81ac111880d58b46ff8b0a8d9308c8d8f3
Otto Touzil <otto.touzil@gmail.com>
2012-02-21 12:50:57 +0100

    new Tasks: fadeIn + fadeOut +logo fuer intro :)

---
+++ catroid/src/at/tugraz/ist/catroid/tutorial/tasks/TaskFadeIn.java
@@ -0,0 +1,65 @@
+/**
+ *  Catroid: An on-device graphical programming language for Android devices
+ *  Copyright (C) 2010  Catroid development team 
+ *  (<http://code.google.com/p/catroid/wiki/Credits>)
+ *
+ *  This program is free software: you can redistribute it and/or modify
+ *  it under the terms of the GNU General Public License as published by
+ *  the Free Software Foundation, either version 3 of the License, or
+ *  (at your option) any later version.
+ *
+ *  This program is distributed in the hope that it will be useful,
+ *  but WITHOUT ANY WARRANTY; without even the implied warranty of
+ *  MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ *  GNU General Public License for more details.
+ *
+ *  You should have received a copy of the GNU General Public License
+ *  along with this program.  If not, see <http://www.gnu.org/licenses/>.
+ */
+package at.tugraz.ist.catroid.tutorial.tasks;
+
+import at.tugraz.ist.catroid.tutorial.CloudController;
+import at.tugraz.ist.catroid.tutorial.TutorialOverlay;
+
+/**
+ * @author faxxe
+ * 
+ */
+public class TaskFadeIn implements Task {
+
+	/*
+	 * (non-Javadoc)
+	 * 
+	 * @see at.tugraz.ist.catroid.tutorial.tasks.Task#execute(at.tugraz.ist.catroid.tutorial.TutorialOverlay)
+	 */
+	@Override
+	public String execute(TutorialOverlay tutorialOverlay) {
+		// TODO Auto-generated method stub
+		CloudController co = new CloudController();
+		co.fadeIn();
+		return null;
+	}
+
+	/*
+	 * (non-Javadoc)
+	 * 
+	 * @see at.tugraz.ist.catroid.tutorial.tasks.Task#getType()
+	 */
+	@Override
+	public Type getType() {
+		// TODO Auto-generated method stub
+		return null;
+	}
+
+	/*
+	 * (non-Javadoc)
+	 * 
+	 * @see at.tugraz.ist.catroid.tutorial.tasks.Task#getTutorType()
+	 */
+	@Override
+	public Tutor getTutorType() {
+		// TODO Auto-generated method stub
+		return null;
+	}
+
+}
