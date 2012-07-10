947bb938683cb721c04f6609c13e229c010df578
Michael Rohrer <mjr@sbox.tugraz.at>
2012-07-10 20:07:36 +0200

    xxxx

--- catroid/src/at/tugraz/ist/catroid/tutorial/Tutor.java
+++ catroid/src/at/tugraz/ist/catroid/tutorial/Tutor.java
@@ -172,7 +172,7 @@ public class Tutor extends SurfaceObjectTutor implements SurfaceObject {
 		tutorStateHistory.addStateToHistory(currentState);
 	}
 
-	//TODO check, if tutors talk parallel
+	//TODO: check, if tutors talk parallel
 	@Override
 	public void say(String text) {
 		if (tutorBubble != null) {
