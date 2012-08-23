eb6b485a97191b340a03d3ec7eaa15b8ad99cd9a
Jakob Unterkofler <jakob.unterkofler@student.tugraz.at>
2012-08-23 14:33:03 +0200

    added strings and layout files

---
+++ catroid/src/at/tugraz/ist/catroid/ui/dialogs/AddProjectScreenshot.java
@@ -0,0 +1,59 @@
+/**
+ *  Catroid: An on-device graphical programming language for Android devices
+ *  Copyright (C) 2010-2011 The Catroid Team
+ *  (<http://code.google.com/p/catroid/wiki/Credits>)
+ *  
+ *  This program is free software: you can redistribute it and/or modify
+ *  it under the terms of the GNU Affero General Public License as
+ *  published by the Free Software Foundation, either version 3 of the
+ *  License, or (at your option) any later version.
+ *  
+ *  An additional term exception under section 7 of the GNU Affero
+ *  General Public License, version 3, is available at
+ *  http://www.catroid.org/catroid_license_additional_term
+ *  
+ *  This program is distributed in the hope that it will be useful,
+ *  but WITHOUT ANY WARRANTY; without even the implied warranty of
+ *  MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ *  GNU Affero General Public License for more details.
+ *   
+ *  You should have received a copy of the GNU Affero General Public License
+ *  along with this program.  If not, see <http://www.gnu.org/licenses/>.
+ */
+package at.tugraz.ist.catroid.ui.dialogs;
+
+import android.app.AlertDialog;
+import android.app.Dialog;
+import android.content.Context;
+import android.view.LayoutInflater;
+import android.view.View;
+import at.tugraz.ist.catroid.R;
+
+public class AddProjectScreenshot {
+	private Context context;
+	Dialog addProjectScreenshot;
+
+	public AddProjectScreenshot(Context context) {
+		this.context = context;
+	}
+
+	public Dialog createDialog() {
+		AlertDialog.Builder builder = new AlertDialog.Builder(context);
+		builder.setTitle(context.getString(R.string.add_project_screensht_titel));
+
+		LayoutInflater inflater = (LayoutInflater) context.getSystemService(Context.LAYOUT_INFLATER_SERVICE);
+		View view = inflater.inflate(R.layout.dialog_add_project_screenshot, null);
+
+		builder.setView(view);
+
+		addProjectScreenshot = builder.create();
+		addProjectScreenshot.setCanceledOnTouchOutside(true);
+
+		return addProjectScreenshot;
+	}
+
+	public void handleCancelButton() {
+		addProjectScreenshot.dismiss();
+	}
+
+}
