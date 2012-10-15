6724d3a0ef6889ade74d7a21a6981210b5cd7542
Robert Painsi <robertpainsi@gmail.com>
2012-10-15 14:35:14 +0200

    Implement PhysicCostumeTest.

--- catroidTest/src/org/catrobat/catroid/test/physics/PhysicCostumeTest.java
+++ catroidTest/src/org/catrobat/catroid/test/physics/PhysicCostumeTest.java
@@ -1,16 +1,13 @@
 package org.catrobat.catroid.test.physics;
 
-import org.catrobat.catroid.content.Costume;
 import org.catrobat.catroid.content.Sprite;
 import org.catrobat.catroid.physics.PhysicCostume;
 import org.catrobat.catroid.physics.PhysicObject;
 import org.catrobat.catroid.physics.PhysicWorld;
-import org.catrobat.catroid.test.utils.TestUtils;
 
 import android.test.AndroidTestCase;
 
-import com.badlogic.gdx.physics.box2d.Body;
-import com.badlogic.gdx.physics.box2d.World;
+import com.badlogic.gdx.math.Vector2;
 import com.badlogic.gdx.utils.GdxNativesLoader;
 
 public class PhysicCostumeTest extends AndroidTestCase {
@@ -29,49 +26,126 @@ public class PhysicCostumeTest extends AndroidTestCase {
 	}
 
 	public void testCheckImageChanged() {
-		Sprite sprite = new Sprite("TestSprite");
-		PhysicWorld physicWorld = new PhysicWorldMock();
-		PhysicObject physicObject = new PhysicObjectMock();
+		PhysicWorldMock physicWorld = new PhysicWorldMock();
+		PhysicCostumeMock physicCostume = new PhysicCostumeMock(null, physicWorld, null);
 
-		PhysicCostume costume = new PhysicCostume(sprite, physicWorld, physicObject);
-		TestUtils.invokeMethod(costume, "checkImageChanged", null, null);
+		physicCostume.setImageChanged(false);
+		boolean checkImageChangedReturnValue = physicCostume.checkImageChanged();
+
+		assertFalse(checkImageChangedReturnValue);
+		assertFalse(physicWorld.changeCostumeExecuted);
+
+		physicCostume.setImageChanged(true);
+		checkImageChangedReturnValue = physicCostume.checkImageChanged();
+
+		assertTrue(checkImageChangedReturnValue);
+		assertTrue(physicWorld.changeCostumeExecuted);
 	}
 
 	public void testUpdatePositionAndRotation() {
+		Sprite sprite = new Sprite("TestSprite");
+		PhysicWorld physicWorld = new PhysicWorld();
+		PhysicObject physicObject = physicWorld.getPhysicObject(sprite);
+		PhysicCostumeUpdateMock physicCostume = new PhysicCostumeUpdateMock(sprite, physicWorld, physicObject);
+
+		Vector2 position = new Vector2(1.2f, 3.4f);
+		float rotation = 3.14f;
+
+		physicCostume.setXYPosition(position.x, position.y);
+		physicCostume.setRotation(rotation);
+
+		assertNotSame(position, physicCostume.getCostumePosition());
+		assertNotSame(rotation, physicCostume.getCostumeRotation());
+
+		physicCostume.updatePositionAndRotation();
+
+		assertEquals(position, physicCostume.getCostumePosition());
+		assertEquals(rotation, physicCostume.getCostumeRotation());
 	}
 
 	public void testPositionAndAngle() {
+		PhysicWorld physicWorld = new PhysicWorld();
+		PhysicObject physicObject = physicWorld.getPhysicObject(new Sprite("TestSprite"));
+		PhysicCostume physicCostume = new PhysicCostume(null, physicWorld, physicObject);
+
+		float[] values = { 1.2f, -3.4f, 5.6f, 7.8f, -9.0f };
+		assertEquals(5, values.length);
+
+		float x = 1.2f;
+		physicCostume.setXPosition(x);
+		assertEquals(x, physicObject.getXPosition());
+
+		float y = -3.4f;
+		physicCostume.setYPosition(y);
+		assertEquals(y, physicObject.getYPosition());
+
+		x = 5.6f;
+		y = 7.8f;
+		physicCostume.setXYPosition(x, y);
+		assertEquals(new Vector2(x, y), physicObject.getXYPosition());
+
+		float rotation = 9.0f;
+		physicCostume.setRotation(rotation);
+		assertEquals(rotation, physicObject.getAngle());
+
+		assertEquals(x, physicCostume.getXPosition());
+		assertEquals(y, physicCostume.getYPosition());
+		assertEquals(rotation, physicCostume.getRotation());
 	}
 
 	public void testSize() {
+		PhysicWorldMock physicWorld = new PhysicWorldMock();
+		PhysicCostume physicCostume = new PhysicCostume(null, physicWorld, null);
+
+		assertFalse(physicWorld.changeCostumeExecuted);
+		physicCostume.setSize(3.14f);
+		assertTrue(physicWorld.changeCostumeExecuted);
 	}
 
-	private class CostumeMock extends Costume {
+	private class PhysicCostumeUpdateMock extends PhysicCostume {
 
-		private boolean imageChanged;
+		public PhysicCostumeUpdateMock(Sprite sprite, PhysicWorld physicWorld, PhysicObject physicObject) {
+			super(sprite, physicWorld, physicObject);
+		}
 
-		public CostumeMock(Sprite sprite) {
-			super(sprite);
+		public Vector2 getCostumePosition() {
+			float x = super.getXPosition();
+			float y = super.getYPosition();
+
+			return new Vector2(x, y);
+		}
+
+		public float getCostumeRotation() {
+			return super.getRotation();
+		}
+	}
+
+	private class PhysicCostumeMock extends PhysicCostume {
+
+		public PhysicCostumeMock(Sprite sprite, PhysicWorld physicWorld, PhysicObject physicObject) {
+			super(sprite, physicWorld, physicObject);
+		}
+
+		@Override
+		protected boolean checkImageChanged() {
+			return super.checkImageChanged();
+		}
+
+		public void setImageChanged(boolean imageChanged) {
+			this.imageChanged = imageChanged;
 		}
 
 	}
 
 	private class PhysicWorldMock extends PhysicWorld {
 
-	}
+		public boolean changeCostumeExecuted = false;
 
-	private class PhysicObjectMock extends PhysicObject {
-
-		public PhysicObjectMock() {
-			super(new BodyMock(null, 0));
-		}
-	}
-
-	private class BodyMock extends Body {
-
-		protected BodyMock(World world, long addr) {
-			super(world, addr);
+		@Override
+		public void changeCostume(Sprite sprite) {
+			changeCostumeExecuted = true;
 		}
 
 	}
+
 }
