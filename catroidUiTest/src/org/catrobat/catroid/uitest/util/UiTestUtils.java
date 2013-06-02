77b84916f2eb90a1e407113876e22c543a502e60
Sumann Florian <sumannf@gmail.com>
2013-06-02 19:33:42 +0200

    WhenKeyBrickTest

--- catroidUiTest/src/org/catrobat/catroid/uitest/util/UiTestUtils.java
+++ catroidUiTest/src/org/catrobat/catroid/uitest/util/UiTestUtils.java
@@ -346,6 +346,7 @@ public class UiTestUtils {
 		brickCategoryMap.put(R.string.brick_if_begin, R.string.category_control);
 		brickCategoryMap.put(R.string.brick_change_variable, R.string.category_control);
 		brickCategoryMap.put(R.string.brick_set_variable, R.string.category_control);
+		//brickCategoryMap.put(R.string.brick_when_key_pressed, R.string.category_control);
 
 		brickCategoryMap.put(R.string.brick_motor_action, R.string.category_lego_nxt);
 	}
