bbe191d0262053f6521df76ef6a140478e53213a
Philipp Taferner <TafPhil@gmail.com>
2013-06-27 12:29:04 +0200

    add jts-1.13 in catrobat-lib-folder and create stub PhysicShapeBuilderJTS

---
+++ catroid/src/org/catrobat/catroid/physics/shapebuilder/PhysicShapeBuilderJTS.java
@@ -0,0 +1,44 @@
+/**
+ *  Catroid: An on-device visual programming system for Android devices
+ *  Copyright (C) 2010-2013 The Catrobat Team
+ *  (<http://developer.catrobat.org/credits>)
+ *  
+ *  This program is free software: you can redistribute it and/or modify
+ *  it under the terms of the GNU Affero General Public License as
+ *  published by the Free Software Foundation, either version 3 of the
+ *  License, or (at your option) any later version.
+ *  
+ *  An additional term exception under section 7 of the GNU Affero
+ *  General Public License, version 3, is available at
+ *  http://developer.catrobat.org/license_additional_term
+ *  
+ *  This program is distributed in the hope that it will be useful,
+ *  but WITHOUT ANY WARRANTY; without even the implied warranty of
+ *  MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the
+ *  GNU Affero General Public License for more details.
+ *  
+ *  You should have received a copy of the GNU Affero General Public License
+ *  along with this program.  If not, see <http://www.gnu.org/licenses/>.
+ */
+package org.catrobat.catroid.physics.shapebuilder;
+
+import org.catrobat.catroid.common.LookData;
+
+import com.badlogic.gdx.physics.box2d.Shape;
+
+public class PhysicShapeBuilderJTS implements PhysicShapeBuilderStrategy {
+
+	/*
+	 * (non-Javadoc)
+	 * 
+	 * @see
+	 * org.catrobat.catroid.physics.shapebuilder.PhysicShapeBuilderStrategy#build(org.catrobat.catroid.common.LookData)
+	 */
+	@Override
+	public Shape[] build(LookData lookData) {
+		// TODO Auto-generated method stub
+
+		return null;
+	}
+
+}
