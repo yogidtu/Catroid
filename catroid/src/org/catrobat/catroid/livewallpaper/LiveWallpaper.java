2a59d254a64920fdf527b96255f6271555316e42
Majda Osmic <mobearette@gmail.com>
2013-11-22 16:46:27 +0100

    Added the correct setting of the screen size values for compatibility with home screen on KitKat

--- catroid/src/org/catrobat/catroid/livewallpaper/LiveWallpaper.java
+++ catroid/src/org/catrobat/catroid/livewallpaper/LiveWallpaper.java
@@ -22,6 +22,7 @@
  */
 package org.catrobat.catroid.livewallpaper;
 
+import android.annotation.SuppressLint;
 import android.content.Context;
 import android.content.Intent;
 import android.content.SharedPreferences;
@@ -46,6 +47,7 @@ import org.catrobat.catroid.stage.PreStageActivity;
 import org.catrobat.catroid.stage.StageListener;
 import org.catrobat.catroid.utils.Utils;
 
+@SuppressLint("NewApi")
 public class LiveWallpaper extends AndroidLiveWallpaperService {
 
 	private StageListener lastCreatedStageListener;
@@ -60,10 +62,6 @@ public class LiveWallpaper extends AndroidLiveWallpaperService {
 	public void onCreate() {
 		//android.os.Debug.waitForDebugger();
 		super.onCreate();
-		DisplayMetrics displayMetrics = new DisplayMetrics();
-		((WindowManager) getSystemService(WINDOW_SERVICE)).getDefaultDisplay().getMetrics(displayMetrics);
-		ScreenValues.SCREEN_WIDTH = displayMetrics.widthPixels;
-		ScreenValues.SCREEN_HEIGHT = displayMetrics.heightPixels;
 		SharedPreferences sharedPreferences = PreferenceManager.getDefaultSharedPreferences(getApplicationContext());
 		SoundManager.getInstance().soundDisabledByLwp = sharedPreferences.getBoolean(Constants.PREF_SOUND_DISABLED,
 				false);
@@ -78,6 +76,8 @@ public class LiveWallpaper extends AndroidLiveWallpaperService {
 
 	@Override
 	public ApplicationListener createListener(boolean isPreview) {
+		setScreenSize(isPreview);
+
 		if (isPreview) {
 			previewEngine = lastCreatedWallpaperEngine;
 		} else {
@@ -110,6 +110,18 @@ public class LiveWallpaper extends AndroidLiveWallpaperService {
 		lastCreatedStageListener = new StageListener(true);
 		lastCreatedWallpaperEngine = new LiveWallpaperEngine(this.lastCreatedStageListener);
 		return lastCreatedWallpaperEngine;
+	}
+
+	private void setScreenSize(boolean isPreview) {
+		DisplayMetrics displayMetrics = new DisplayMetrics();
+		int currentApiVersion = android.os.Build.VERSION.SDK_INT;
+		if (!isPreview && currentApiVersion >= 19) {
+			((WindowManager) getSystemService(WINDOW_SERVICE)).getDefaultDisplay().getRealMetrics(displayMetrics);
+		} else {
+			((WindowManager) getSystemService(WINDOW_SERVICE)).getDefaultDisplay().getMetrics(displayMetrics);
+		}
+		ScreenValues.SCREEN_WIDTH = displayMetrics.widthPixels;
+		ScreenValues.SCREEN_HEIGHT = displayMetrics.heightPixels;
 	}
 
 	@Override
