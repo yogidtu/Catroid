8bc64380cc36b3e149fdd3cb299dab2dba058191
Manuel Wallner <wallner.manuel@gmail.com>
2013-11-27 12:46:39 +0000

    Add getView in PrintBrick for enhanced debugging experience

--- catroidCucumberTest/src/org/catrobat/catroid/test/cucumber/util/PrintBrick.java
+++ catroidCucumberTest/src/org/catrobat/catroid/test/cucumber/util/PrintBrick.java
@@ -22,8 +22,17 @@
  */
 package org.catrobat.catroid.test.cucumber.util;
 
+import android.content.Context;
+import android.graphics.drawable.Drawable;
+import android.view.View;
+import android.widget.BaseAdapter;
+import android.widget.CompoundButton;
+import android.widget.CompoundButton.OnCheckedChangeListener;
+import android.widget.TextView;
+
 import com.badlogic.gdx.scenes.scene2d.actions.SequenceAction;
 
+import org.catrobat.catroid.R;
 import org.catrobat.catroid.content.Script;
 import org.catrobat.catroid.content.Sprite;
 import org.catrobat.catroid.content.bricks.Brick;
@@ -59,6 +68,30 @@ public class PrintBrick extends BrickBaseType {
 	}
 
 	@Override
+	public View getView(Context context, int brickId, BaseAdapter baseAdapter) {
+		if (animationState) {
+			return view;
+		}
+		view = View.inflate(context, R.layout.brick_print, null);
+		view = getViewWithAlpha(alphaValue);
+
+		setCheckboxView(R.id.brick_print_checkbox);
+		final Brick brickInstance = this;
+		checkbox.setOnCheckedChangeListener(new OnCheckedChangeListener() {
+			@Override
+			public void onCheckedChanged(CompoundButton buttonView, boolean isChecked) {
+				checked = isChecked;
+				adapter.handleCheck(brickInstance, isChecked);
+			}
+		});
+
+		TextView textView = (TextView) view.findViewById(R.id.brick_print_text);
+		textView.setText(this.text);
+
+		return view;
+	}
+
+	@Override
 	public Brick copyBrickForSprite(Sprite sprite, Script script) {
 		PrintBrick copyBrick = (PrintBrick) clone();
 		copyBrick.sprite = sprite;
@@ -66,10 +99,30 @@ public class PrintBrick extends BrickBaseType {
 	}
 
 	@Override
+	public View getViewWithAlpha(int alphaValue) {
+		if (view != null) {
+			View layout = view.findViewById(R.id.brick_print_layout);
+			Drawable background = layout.getBackground();
+			background.setAlpha(alphaValue);
+			this.alphaValue = (alphaValue);
+
+			TextView printLabel = (TextView) view.findViewById(R.id.brick_print_label);
+			printLabel.setTextColor(printLabel.getTextColors().withAlpha(alphaValue));
+		}
+
+		return view;
+	}
+
+	@Override
 	public Brick clone() {
 		PrintBrick brick = new PrintBrick(sprite, text);
 		brick.outputStream = this.outputStream;
 		return brick;
+	}
+
+	@Override
+	public View getPrototypeView(Context context) {
+		return View.inflate(context, R.layout.brick_print, null);
 	}
 
 	@Override
